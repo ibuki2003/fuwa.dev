842aef4a23a59c3dba499052bd9a926c5055fb35
ibuki2003 <main@fuwa.dev>
2021-04-04 18:26:47 +0900

    chore: remove default unused components

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
 
 // Your top level component
-import App from './App'
+import App from "./App";
 
 // Export your top level component as JSX (for static rendering)
-export default App
+export default App;
 
+const target = document?.getElementById("root");
 // Render your app
-if (typeof document !== 'undefined') {
-  const target = document.getElementById('root')
+if (target) {
 
   const renderMethod = target.hasChildNodes()
     ? ReactDOM.hydrate
-    : ReactDOM.render
+    : ReactDOM.render;
 
-  const render = (Comp: Function) => {
+    const render = (Comp: React.FC) => {
     renderMethod(
       <AppContainer>
         <Comp />
       </AppContainer>,
       target
-    )
-  }
+    );
+  };
 
   // Render!
-  render(App)
+  render(App);
 
   // Hot Module Replacement
   if (module && module.hot) {
-    module.hot.accept('./App', () => {
-      render(App)
-    })
+    module.hot.accept("./App", () => {
+      render(App);
+    });
   }
 }
