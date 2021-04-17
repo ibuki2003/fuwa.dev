15352c2de13e83f1cbc8c97c0be3246404a5c369
ibuki2003 <main@fuwa.dev>
2021-04-18 00:19:28 +0900

    fix(prevent-inline): fail on build

--- plugins/prevent-inline/node.api.js
+++ plugins/prevent-inline/node.api.js
@@ -1,12 +1,16 @@
 export default () => ({
-  webpack: config => {
-    config.module.rules = config.module.rules.map(r => {
-      if ('oneOf' in r) r.oneOf = r.oneOf.map(i => {
-        if (i.loader === 'url-loader') i.query.limit = false; // disable always
-        return i;
-      });
+  webpack: (config) => {
+    config.module.rules = config.module.rules.map((r) => {
+      if ("oneOf" in r)
+        r.oneOf = r.oneOf.map((i) => {
+          if (i.loader === "url-loader") {
+            if ("options" in i) i.options.limit = false;
+            if ("query" in i) i.query.limit = false;
+          }
+          return i;
+        });
       return r;
     });
     return config;
-  }
-})
+  },
+});
