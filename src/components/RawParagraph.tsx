d56631ef393e0c634699036c5a54d99444454c65
ibuki2003 <main@fuwa.dev>
2022-01-21 14:17:56 +0900

    fix: add key props in RawParagraph

--- src/components/RawParagraph.tsx
+++ src/components/RawParagraph.tsx
@@ -4,18 +4,21 @@ interface Props {
   children: React.ReactNode | Array<React.ReactNode>;
 }
 
-function nl2br(c: React.ReactNode) {
+function nl2br(c: React.ReactNode, i: number) {
   if (typeof c !== "string") return c;
-  return c.split("\n").flatMap<React.ReactNode[]>((frag, i) => {
-    // eslint-disable-next-line react/jsx-key
-    if (i > 0) return [<br />, frag];
-    return [frag];
+  return c.split("\n").flatMap<React.ReactNode[]>((frag, j) => {
+    if (j > 0)
+      return [
+        <br key={`${i}-${j}-0`} />,
+        <React.Fragment key={`${i}-${j}-1`}>{frag}</React.Fragment>,
+      ];
+    return [<React.Fragment key={`${i}-${j}`}>{frag}</React.Fragment>];
   });
 }
 
 const RawParagraph: React.FC<Props> = ({ children }) => {
   const new_children = React.useMemo(() => {
-    if (!Array.isArray(children)) return nl2br(children);
+    if (!Array.isArray(children)) return nl2br(children, 0);
     return children.flatMap(nl2br);
   }, [children]);
   return <p>{new_children}</p>;
