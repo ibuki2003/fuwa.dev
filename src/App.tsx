eb56121813efe07fc6425c4f12035bda5ffb18d1
ibuki2003 <main@fuwa.dev>
2022-02-27 16:54:43 +0900

    feat: remove footer links

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,6 @@ import Gtag from "components/Gtag";
 
 import "./app.scss";
 
-import twitter_icon from "assets/footer_icons/twitter.svg";
-import github_icon from "assets/footer_icons/github.svg";
-import zenn_icon from "assets/footer_icons/zenn.svg";
-import qiita_icon from "assets/footer_icons/qiita.svg";
-import telegram_icon from "assets/footer_icons/telegram.svg";
-import stackoverflow_icon from "assets/footer_icons/stackoverflow.svg";
-
-interface FooterLink {
-  name: string;
-  url: string;
-  image: string;
-}
-const footer_links: FooterLink[] = [
-  {
-    name: "twitter",
-    url: "https://twitter.com/ibuki2003",
-    image: twitter_icon,
-  },
-  { name: "github", url: "https://github.com/ibuki2003", image: github_icon },
-  { name: "zenn", url: "https://zenn.dev/fuwa2003", image: zenn_icon },
-  { name: "qiita", url: "https://qiita.com/ibuki2003", image: qiita_icon },
-  { name: "telegram", url: "https://t.me/ibuki2003", image: telegram_icon },
-  {
-    name: "stackoverflow",
-    url: "https://stackoverflow.com/users/11062133/ibuki2003",
-    image: stackoverflow_icon,
-  },
-];
-
 const App: React.FC = () => {
   return (
     <Root>
@@ -67,15 +38,6 @@ const App: React.FC = () => {
         </main>
         <footer>
           <small>ibuki2003</small>
-          <ul>
-            {footer_links.map((item) => (
-              <li key={item.name}>
-                <a href={item.url} aria-label={item.name}>
-                  <img src={item.image} />
-                </a>
-              </li>
-            ))}
-          </ul>
         </footer>
       </React.Suspense>
     </Root>
