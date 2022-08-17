3cd2d3947c71f9c4dc3e7c63aaa1d0790e64b790
ibuki2003 <main@fuwa.dev>
2022-08-17 17:55:12 +0900

    feat: migrate page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import "app.scss";
 import "katex/dist/katex.min.css";
 import "highlight.js/scss/default.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>
+        <a href="/" className="navbar-brand">
+          ふわわあのへや
+        </a>
+        <small>競プロとか、好きなこと、いろいろ。</small>
+      </header>
+      <main id="md">
+        <Component {...pageProps} />
+      </main>
+      <footer>
+        <small>ibuki2003</small>
+      </footer>
+    </>
+  );
 }
 
 export default MyApp;
