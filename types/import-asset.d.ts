03cca0f170834cf3447b9a6f93b1baa74749f28f
ibuki2003 <main@fuwa.dev>
2022-08-17 17:23:05 +0900

    build: install sass

--- types/import-asset.d.ts
+++ types/import-asset.d.ts
@@ -3,3 +3,7 @@ declare module "*.jpeg" { const name: string; export default name; }
 declare module "*.png" { const name: string; export default name; }
 declare module "*.gif" { const name: string; export default name; }
 declare module "*.svg" { const name: string; export default name; }
+
+declare module "*.css" { const content: Record<string, string>; export default content; }
+declare module "*.scss" { const content: Record<string, string>; export default content; }
+declare module "*.sass" { const content: Record<string, string>; export default content; }
