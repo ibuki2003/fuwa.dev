480eb01ef6f3674f921747da25c7913e6d986977
ibuki2003 <main@fuwa.dev>
2022-08-17 19:54:19 +0900

    feat: place gtag

---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const gtag_id = "G-SY78XL8BN4";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {process.env.NODE_ENV == "production" && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gtag_id}`}
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${gtag_id}');`,
+              }}
+            />
+          </>
+        )}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
