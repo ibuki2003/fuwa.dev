22387df726ef7af6afcaf0321562372bec0a7d80
ibuki2003 <main@fuwa.dev>
2023-02-24 21:08:05 +0900

    chore: add matomo tracking code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,22 @@ gtag('js', new Date());
 gtag('config', '${gtag_id}');`,
               }}
             />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.fuwa.dev/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();`,
+              }}
+            />
           </>
         )}
         <script async src="https://platform.twitter.com/widgets.js" />
