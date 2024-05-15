463d8f8739557669bdbb37f056bf6504b80d9db9
ibuki2003 <main@fuwa.dev>
2024-05-14 21:18:05 -1000

    build: install astro

--- types/import-asset.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.jpg" { const name: string; export default name; }
-declare module "*.jpeg" { const name: string; export default name; }
-declare module "*.png" { const name: string; export default name; }
-declare module "*.gif" { const name: string; export default name; }
-declare module "*.svg" { const name: string; export default name; }
-
-declare module "*.css" { const content: Record<string, string>; export default content; }
-declare module "*.scss" { const content: Record<string, string>; export default content; }
-declare module "*.sass" { const content: Record<string, string>; export default content; }
