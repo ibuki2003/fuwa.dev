157729f2600a7ae033c306fdda1f10f3ab2a9fa5
ibuki2003 <main@fuwa.dev>
2024-06-01 19:15:40 +0900

    style: prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,13 @@
 import { z, defineCollection } from "astro:content";
 
 export const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string().optional(),
-        date: z.date(),
-    }),
+  type: "content",
+  schema: z.object({
+    title: z.string().optional(),
+    date: z.date(),
+  }),
 });
 
 export const collections = {
-    posts: postsCollection,
+  posts: postsCollection,
 };
-
-
