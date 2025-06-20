7a15f6d7810c1362e670ed824ef290ef4f866a48
ibuki2003 <main@fuwa.dev>
2025-06-20 21:28:51 +0900

    chore: update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import remarkBreaks from 'remark-breaks';
 
 import mdx from "@astrojs/mdx";
 
+import { visit } from "unist-util-visit";
+import { h } from 'hastscript';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -20,6 +23,7 @@ export default defineConfig({
       remarkBreaks,
       myMarkdownPlugin,
       remarkMath,
+      remarkImgAttrs,
     ],
     rehypePlugins: [
       rehypeSlug,
@@ -51,3 +55,77 @@ function myMarkdownPlugin () {
     }
   }
 }
+
+function remarkImgAttrs() {
+  function replace(source, target) {
+    for (const property in source) { delete source[property]; }
+    Object.assign(source, target);
+  }
+
+  function parseAttrs(alt) {
+    const attrs = {};
+    // "ALT TEXT {key1=value1, key2=value2}"
+    const m = alt.match(/^(.*?)\s*{([^}]+)}$/);
+    if (m) {
+      const attrStr = m[2];
+      const attrPairs = attrStr.split(',').map((pair) => pair.trim());
+      for (const pair of attrPairs) {
+        const pos = pair.indexOf('=');
+        if (pos === -1) {
+          attrs[pair] = true;
+        } else {
+          const key = pair.slice(0, pos).trim();
+          const value = pair.slice(pos + 1).trim();
+          attrs[key] = value;
+        }
+      }
+      return { attrs, alt: m[1] };
+    }
+    return { attrs: {}, alt };
+  }
+
+  return (tree) => {
+    visit(tree, 'image', (node) => {
+      if (!node.alt) return;
+      const { attrs, alt } = parseAttrs(node.alt);
+
+      node.alt = alt;
+
+      if (!attrs) return;
+
+      const caption = attrs.caption || (attrs.caption_alt && alt);
+
+      if (caption) {
+        const figElm = {
+          type: 'element',
+          data: { hName: 'figure' },
+          children: [
+            { ...node },
+            {
+              type: 'element',
+              data: { hName: 'figcaption' },
+              children: [{ type: 'text', value: caption, }],
+            },
+          ],
+        };
+        replace(node, figElm);
+      }
+
+      if (attrs.width) {
+        // set style
+        node.data ??= {};
+        node.data.hProperties = node.data.hProperties || {};
+        node.data.hProperties.style = node.data.hProperties.style || '';
+        node.data.hProperties.style += `width: ${attrs.width};`;
+      }
+      if (attrs.maxWidth) {
+        // set style
+        node.data ??= {};
+        node.data.hProperties = node.data.hProperties || {};
+        node.data.hProperties.style = node.data.hProperties.style || '';
+        node.data.hProperties.style += `max-width: ${attrs.maxWidth};`;
+      }
+    });
+  };
+}
+
